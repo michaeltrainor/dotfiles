2f7edcfce83726da027b066a60612ad46b6ad523
mtd3v <accounts@michaeltrainor.com>
2024-08-06 11:17:42 -0400

    refactored for gnu stow

---
+++ nvim/.config/nvim/lua/plugins/example.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- add tsx and treesitter
+      vim.list_extend(opts.ensure_installed, {
+        "tsx",
+        "typescript",
+      })
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+      },
+    },
+  },
+}
